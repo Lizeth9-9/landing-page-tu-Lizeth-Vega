dcf5b29faa57afc23a21fde1e0ef1f237207ce71
Lizeth9-9 <lili.prog0@gmail.com>
2026-07-03 20:52:26 +0200

    css 95 html 98

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
+const botonMenu = document.querySelector ('.fa-bars')
+const menu = document.querySelector ('.menu-hide')
+botonMenu.addEventListener ('click', () => {    
+menu.classList.toggle('activo')
 
+
+});
