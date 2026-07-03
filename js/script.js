dd85014d59526e3ddf0abc39731838d64de87aeb
Lizeth9-9 <lili.prog0@gmail.com>
2026-07-03 20:59:16 +0200

    JS MENU HAMBURGUESA Y CSS

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
+//MENU HAMBURGUESA //
+
 const botonMenu = document.querySelector ('.fa-bars')
 const menu = document.querySelector ('.menu-hide')
 botonMenu.addEventListener ('click', () => {    
 menu.classList.toggle('activo')
-
-
 });
