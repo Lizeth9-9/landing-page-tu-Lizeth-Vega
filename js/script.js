de0aab7b338690a3877f51e4f70840002b1a561f
Lizeth9-9 <lili.prog0@gmail.com>
2026-07-06 11:55:13 +0200

    validacion de formulario JS

--- js/script.js
+++ js/script.js
@@ -1,7 +1,73 @@
 //MENU HAMBURGUESA //
 
-const botonMenu = document.querySelector ('.fa-bars')
-const menu = document.querySelector ('.menu-hide')
-botonMenu.addEventListener ('click', () => {    
-menu.classList.toggle('activo')
+const botonMenu = document.querySelector('.fa-bars')
+const menu = document.querySelector('.menu-hide')
+botonMenu.addEventListener('click', () => {
+    menu.classList.toggle('activo')
 });
+
+//FORMULARIO//
+
+const form = document.querySelector('#fcontacto');
+const nombre = document.querySelector('#nombre');
+const email = document.querySelector('#email');
+const movil = document.querySelector('#movil');
+const errorNombre = document.querySelector('#error-nombre');
+const erroremail = document.querySelector('#error-email');
+const errormovil = document.querySelector('#error-movil');
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const nombreValor = nombre.value.trim();
+    const emailValor = email.value.trim();
+    const movilValor = movil.value.trim();
+
+    let error = false;
+
+    if (nombreValor === '') {
+        nombre.classList.add('error');
+        errorNombre.classList.add('visible');
+        error = true;
+    }
+
+    if (emailValor === '') {
+        email.classList.add('error');
+        erroremail.classList.add('visible');
+        error = true;
+    }
+
+    if (movilValor === '') {
+        movil.classList.add('error');
+        errormovil.classList.add('visible');
+        error = true;
+    }
+
+    if (!error) {
+
+        form.reset();
+    }
+});
+
+
+
+
+
+
+
+
+// if (email === '') {
+//     document.querySelector('#erroremail').textContent = "Debe rellenar E-mail"
+//     error = true;
+// }
+// else
+//     if (error) return;
+
+
+
+
+
+
+
+
